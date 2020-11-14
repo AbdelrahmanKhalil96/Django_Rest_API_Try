61b356c629c2990afbdd4f90c5ac1c1df3e7c9aa
Abdelrahman Khalil <AbdelrahmanKhalil96@gmail.com>
2020-11-14 14:55:59 +0200

    Project_firstEdit_restfulAPI

--- MyProject/api_basic/serializers.py
+++ MyProject/api_basic/serializers.py
@@ -5,7 +5,8 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'author']
+        ###fields = ['id', 'title', 'author']
+        fields = '__all__'
     # title = serializers.CharField(max_length=50)
     # author = serializers.CharField(max_length=50)
     # email = serializers.EmailField(max_length=50)
